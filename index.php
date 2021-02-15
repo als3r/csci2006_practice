938bc58d8945705de2765cc55b29109cd2d605a1
Alex <sergeyev.alex@gmail.com>
2021-02-14 22:22:38 -0600

    add a new page account with user form

--- index.php
+++ index.php
@@ -1,37 +1,35 @@
 <?php
-  $page = getPage();
-  echo $page;
+
+  $page = isset($_GET['page']) && in_array($_GET['page'], [
+      'artwork',
+      'account'
+    ])? $_GET['page'] : 'artwork';
+
+  echo getPage($page);
+
   exit;
 
 /**
 * Get full page contents
 */
-function getPage(){
-  $page = '<!DOCTYPE html>
-    <html lang="en">';
-  $page .= getHead();
-  $page .= getBody();
-  $page .= '</html>';
-  return $page;
-}
+function getPage($page = 'artwork'){
 
-/**
-* Get Title of the page
-*/
-function getTitle(){
-  return 'Lebrun - Self-portrait in a Straw Hat';
+  $output = '';
+  $output .= getHead($page);
+  $output .= getBody($page);
+  return $output;
 }
 
 /**
 * Get contents of the head tag
 */
-function getHead(){
+function getHead($page){
   $head = '
   <!DOCTYPE html>
   <html lang="en">
   <head>
       <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-      <title>' . getTitle() . '</title>
+      <title>' . getTitle($page) . '</title>
       <link rel="stylesheet" href="_aux/default.css">
   </head>
   ';
@@ -39,20 +37,44 @@ function getHead(){
 }
 
 /**
+* Get Title of the page
+*/
+function getTitle($page){
+  if($page == 'artwork'){
+    return 'Lebrun - Self-portrait in a Straw Hat';
+  } else if($page == 'account') {
+    return 'Account Details';
+  } else {
+    return '';
+  }
+}
+
+/**
 * Get contents of the body tag
 */
-function getBody(){
-  $body = '
-  <body>'.
-        getHeader()
-      .'
-      <main>
-          ' . getMainArticleSection() . '
-          <h2>Similar Artwork</h2>
-          ' . getRelatedItemsSection() . '
-      </main>
-      ' . getFooter() . '
-  </body>';
+function getBody($page = ''){
+  $body  = '<body>';
+  $body .= getHeader();
+
+  $body .= '<main>';
+
+  if($page == 'account'){
+
+    $body .= accountDetails();
+
+  } elseif ($page == 'artwork'){
+
+    $body .= getMainArticleSection();
+    $body .= '<h2>Similar Artwork</h2>';
+    $body .= getRelatedItemsSection();
+
+  }
+
+  $body .= '</main>';
+
+  $body .= getFooter();
+  $body .= '</body>';
+  $body .= '</html>';
   return $body;
 }
 
@@ -63,7 +85,7 @@ function getHeader() {
   $header = '<header>
       <nav class="user">
           <ul>
-              <li><a href="#">My Account</a></li>
+              <li><a href="index.php?page=account">My Account</a></li>
               <li><a href="#">Wish List</a></li>
               <li><a href="#">Shopping Cart</a></li>
           </ul>
@@ -71,7 +93,7 @@ function getHeader() {
       <h1>Art Store</h1>
       <nav>
           <ul>
-              <li><a href="#">Home</a></li>
+              <li><a href="index.php?page=artwork">Home</a></li>
               <li><a href="#">About Us</a></li>
               <li><a href="#">Art Works</a></li>
               <li><a href="#">Artists</a></li>
@@ -188,5 +210,57 @@ function getFooter(){
       <p>All images are copyright to their owners. This is just a hypothetical site ©2020 Copyright Art Store</p>
   </footer>';
   return $footer;
+}
+
+
+function accountDetails(){
+
+
+  $first_name = !empty($_POST['userform_first_name']) ? $_POST['userform_first_name'] : '';
+  $last_name  = !empty($_POST['userform_last_name'])  ? $_POST['userform_last_name']  : '';
+  $username   = !empty($_POST['userform_username'])   ? $_POST['userform_username']   : '';
+  $password   = !empty($_POST['userform_password'])   ? $_POST['userform_password']   : '';
+  $address1   = !empty($_POST['userform_address1'])   ? $_POST['userform_address1']   : '';
+  $address2   = !empty($_POST['userform_address2'])   ? $_POST['userform_address2']   : '';
+  $city       = !empty($_POST['userform_city'])       ? $_POST['userform_city']       : '';
+  $state      = !empty($_POST['userform_state'])      ? $_POST['userform_state']      : '';
+  $zip_code   = !empty($_POST['userform_zip'])        ? $_POST['userform_zip']        : '';
+
+
+  $output = '';
+  $output .= '<form class="userForm" action="index.php?page=account" method="POST">';
+
+  $output .= '<h4 class="userForm-header">Account Details</h4>';
+
+  $output .= '<label for="userFormFirstName"  class="form-label" >First Name</label>';
+  $output .= '<input id="userFormFirstName" name="userform_first_name" type="text" value="'.$first_name.'" class="form-input" />';
+
+  $output .= '<label for="userFormLastName"  class="form-label" >Last Name</label>';
+  $output .= '<input id="userFormLastName" name="userform_last_name" type="text" value="'.$last_name.'" class="form-input" />';
+
+  $output .= '<label for="userFormUsername"  class="form-label" >Username</label>';
+  $output .= '<input id="userFormUsername" name="userform_username" type="text" value="'.$username.'" class="form-input" />';
+
+  $output .= '<label for="userFormPassword"  class="form-label" >Password</label>';
+  $output .= '<input id="userFormPassword" name="userform_password" type="password" value="'.$password.'" class="form-input" />';
+
+  $output .= '<label for="userFormAddress1"  class="form-label" >Address</label>';
+  $output .= '<input id="userFormAddress1" name="userform_address1" type="text" value="'.$address1.'" class="form-input" />';
+  $output .= '<input id="userFormAddress2" name="userform_address2" type="text" value="'.$address2.'" class="form-input" />';
+
+  $output .= '<label for="userFormCity"  class="form-label" >City</label>';
+  $output .= '<input id="userFormCity" name="userform_city" type="text" value="'.$city.'" class="form-input" />';
+
+  $output .= '<label for="userFormState"  class="form-label" >State</label>';
+  $output .= '<input id="userFormState" name="userform_state" type="text" value="'.$state.'" class="form-input" />';
+
+  $output .= '<label for="userFormZip"  class="form-label" >Zip Code</label>';
+  $output .= '<input id="userFormZip" name="userform_zip" type="text" value="'.$zip_code.'" class="form-input" />';
+
+  $output .= '<input type="hidden" value="account" name="page" />';
+  $output .= '<input type="submit" value="Save Changes" name="userform_submit" class="form-submit" >';
+  $output .= '</form>';
+
+  return $output;
 }
 ?>
